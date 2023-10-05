6d85f18913b74c4550de21c296186f2f459fffe3
Regi <gt12139@gmail.com>
2023-10-06 00:04:06 +0300

    Done.

--- index.ts
+++ index.ts
@@ -1,29 +1,33 @@
+export {}
+
 // Create a "close" button and append it to each list item
-const myNodelist = document.getElementsByTagName('LI')
+const myNodelist: HTMLCollectionOf<Element>  = document.getElementsByTagName('LI')
 let i
 for (i = 0; i < myNodelist.length; i++) {
-  const span = document.createElement('SPAN')
-  const txt = document.createTextNode('\u00D7')
+  const span: HTMLElement = document.createElement('SPAN')
+  const txt: Text = document.createTextNode('\u00D7')
   span.className = 'close'
   span.appendChild(txt)
   myNodelist[i].appendChild(span)
 }
+
 // Click on a close button to hide the current list item
-const closeButtons = document.getElementsByClassName('close')
+let closeButtons: HTMLCollectionOf<Element> = document.getElementsByClassName('close')
 for (i = 0; i < closeButtons.length; i++) {
-  const closeButton = closeButtons[i]
+  let closeButton: Element = closeButtons[i]
   closeButton.addEventListener('click', () => {
     const li = closeButton.parentElement
-    if (li) {
+    if (!!li) {
       li.style.display = 'none'
     }
   })
 }
+
 // Add a "checked" symbol when clicking on a list item
-const list = document.querySelector('ul')
+const list: HTMLUListElement | null = document.querySelector('ul')
 if (list) {
   list.addEventListener('click', function (ev) {
-    const target = ev.target
+    let target: HTMLElement = ev.target as HTMLElement
     if (target) {
       if (target.tagName === 'LI') {
         target.classList.toggle('checked')
@@ -31,14 +35,16 @@ if (list) {
     }
   }, false)
 }
+
 // Create a new list item when clicking on the "Add" button
-function newElement () {
-  const li = document.createElement('li')
-  const input = document.getElementById('myInput')
-  if (!input) { return }
+function newElement (): void {
+  const li: HTMLLIElement = document.createElement('li')
+  const input: HTMLInputElement = document.getElementById('myInput') as HTMLInputElement
+  if (!input) return
   const inputValue = input.value
-  const textNode = document.createTextNode(inputValue)
+  const textNode: Text = document.createTextNode(inputValue)
   li.appendChild(textNode)
+
   if (inputValue === '') {
     alert('You must write something!')
   } else {
@@ -48,22 +54,22 @@ function newElement () {
     }
   }
   input.value = ''
+
   const span = document.createElement('SPAN')
   const txt = document.createTextNode('\u00D7')
   span.className = 'close'
   span.appendChild(txt)
   li.appendChild(span)
+
   span.addEventListener('click', () => {
     const li = span.parentElement
-    if (li) {
+    if (!!li) {
       li.style.display = 'none'
     }
   })
 }
-const addBtn = document.getElementById('addBtn')
-addBtn === null || addBtn === void 0
-  ? void 0
-  : addBtn.addEventListener('click', () => {
-    newElement()
-  })
-export {}
+
+const addBtn: HTMLElement | null = document.getElementById('addBtn')
+addBtn?.addEventListener('click', () => {
+  newElement()
+})
